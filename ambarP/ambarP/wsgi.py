a120720f64b483d14480a5db88e122029843e19c
Alejandro <alex.brito.damas@gmail.com>
2020-03-03 11:57:49 -0600

    correcciones

--- ambarP/ambarP/wsgi.py
+++ ambarP/ambarP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ambarDjango project.
+WSGI config for ambarP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambarDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambarP.settings')
 
 application = get_wsgi_application()
